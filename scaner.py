e0ff6eb85c8b39e1d97327ec57350c68c6ccd8f5
YaD-97 <61564653+YaD-97@users.noreply.github.com>
2020-03-17 00:46:09 +0300

    Add files via upload

--- scaner.py
+++ scaner.py
@@ -6,6 +6,7 @@ import openpyxl
 import os
 import datetime
 from openpyxl.styles import PatternFill
+import requests
 
 
 # Заплатка для ошибки с протоколом
@@ -13,18 +14,66 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 
 # Получение сертификата с хоста по порту 443
-def get_cert(hostname):
-    print("")
-    certificate = ssl.get_server_certificate((hostname, 443))
-    # print("get_server_certificate: \n", certificate)
+def get_data(hostname):
+    print("\n\naddress: ", addr)
+    Comment = ""
+    Header = ""
+    Certificate = ""
+    notBefore = ""
+    notAfter = ""
+    Days_left = ""
+    Signed = ""
+    try:
+        if hostname[1] == "443":
+            URL = "https://" + hostname[0]
+        else:
+            URL = "http://" + hostname[0]
+        print(URL)
+        headers = {'headers': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0'}
+        page = requests.get(URL, headers=headers, timeout=10, verify=False)
+        page = page.text
+        try:
+            # print(page)
+            Header = page[page.find('<title>') + 7: page.find('</title>')]
+        except Exception as Ex:
+            Header = "ERROR: " + str(Ex)
+        Availability = "Доступен"
+        if hostname[1] != "443":
+            return [hostname[0] + ":" + hostname[1], Availability, Header, Certificate, notBefore, notAfter, Days_left,
+                    Signed, Comment]
+        else:
+            try:
+                certificate = ssl.get_server_certificate((hostname[0], 443))
+                decode_data = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, certificate)
 
-    decode_data = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, certificate)
+                print("issuer: ", decode_data.get_issuer())
+                print("notAfter: ",
+                      time.mktime(time.strptime(decode_data.get_notAfter().decode('ascii'), '%Y%m%d%H%M%SZ')))
+                print("notBefore: ",
+                      time.mktime(time.strptime(decode_data.get_notBefore().decode('ascii'), '%Y%m%d%H%M%SZ')))
 
-    print("issuer: ", decode_data.get_issuer())
-    print("notAfter: ", time.mktime(time.strptime(decode_data.get_notAfter().decode('ascii'), '%Y%m%d%H%M%SZ')))
-    print("notBefore: ", time.mktime(time.strptime(decode_data.get_notBefore().decode('ascii'), '%Y%m%d%H%M%SZ')))
+                if str(decode_data.get_issuer()).endswith("/CN=TAG-CA'>"):
+                    Signed = "Да"
+                else:
+                    Signed = "Нет"
+                notBefore = format_data(decode_data.get_notBefore())
+                notAfter = format_data(decode_data.get_notAfter())
+                Days_left = days_left(decode_data.get_notAfter())
+                Comment = str(decode_data.get_issuer())
+                Certificate = "Есть"
+            except Exception as Ex:
+                Certificate = "Нет"
+                Comment = str(Ex)
+                return [hostname[0] + ":" + hostname[1], Availability, Header, Certificate, notBefore, notAfter,
+                        Days_left, Signed, Comment]
+    except Exception as Ex:
+        Availability = "Не доступен"
+        Comment = str(Ex)
+        return [hostname[0] + ":" + hostname[1], Availability, Header, Certificate, notBefore, notAfter, Days_left,
+                Signed, Comment]
 
-    return decode_data
+    return [hostname[0] + ":" + hostname[1], Availability, Header, Certificate, notBefore, notAfter, Days_left,
+            Signed, Comment]
 
 
 # Вычисляем оставшиеся дни до конца сертификата
@@ -36,7 +85,7 @@ def days_left(time_):
         days = (time_ - time.time())//86400
     else:
         days = 0
-    return days
+    return int(days)
 
 
 # Форматирование даты
@@ -58,9 +107,7 @@ def get_hostname(file):
 
     for host in root.getchildren():
         if host.tag == "host":
-            if host.ports.port.attrib["portid"] == "443":
-                # print("address: ", host.address.attrib["addr"])
-                addresses.append(host.address.attrib["addr"])
+            addresses.append([host.address.attrib["addr"], host.ports.port.attrib["portid"]])
 
     return addresses
 
@@ -83,21 +130,22 @@ def Excel(results):
         for row in rows:
             x += 1
             ws.append(row)
-            if x != 1 and row[5]:
-                if row[5] == 0:
-                    ws["F" + str(x)].fill = redFill
-                elif row[5] <= 180:
-                    ws["F" + str(x)].fill = orangeFill
+            if x != 1 and row[6]:
+                if row[6] == 0:
+                    ws["G" + str(x)].fill = redFill
+                elif row[6] <= 180:
+                    ws["G" + str(x)].fill = orangeFill
 
         # Ширина ячеек
-        ws.column_dimensions["A"].width = 15
-        ws.column_dimensions["B"].width = 15
-        ws.column_dimensions["C"].width = 15
-        ws.column_dimensions["D"].width = 22
-        ws.column_dimensions["E"].width = 22
-        ws.column_dimensions["F"].width = 14
-        ws.column_dimensions["G"].width = 10
-        ws.column_dimensions["H"].width = 100
+        ws.column_dimensions["A"].width = 18
+        ws.column_dimensions["B"].width = 12
+        ws.column_dimensions["C"].width = 50
+        ws.column_dimensions["D"].width = 12
+        ws.column_dimensions["E"].width = 18
+        ws.column_dimensions["F"].width = 18
+        ws.column_dimensions["G"].width = 14
+        ws.column_dimensions["H"].width = 10
+        ws.column_dimensions["I"].width = 150
 
     # сохранение файла в текущую директорию
     wb.save("results.xlsx")
@@ -117,29 +165,10 @@ results = []
 # Прокручиваем отчеты один за другим
 for file in files:
     addresses = get_hostname("./reports/" + file)
-    result = [["Адрес", "Доступность", "Сертификат", "Действует от", "Годен до", "Осталось дней", "Подписан", "Примечание"]]
+    result = [["Адрес", "Доступность", "Заголовок", "Сертификат", "Действует от", "Годен до", "Осталось дней", "Подписан", "Примечание"]]
     # Стучимся к хостам, проверяем сертификаты и доступность
     for addr in addresses:
-        print("\n\naddress: ", addr)
-        try:
-            decode_data = get_cert(addr)
-            try:
-                if str(decode_data.get_issuer()).endswith("/CN=TAG-CA'>"):
-                    signed = "Да"
-                else:
-                    signed = "Нет"
-                result.append([addr, "Доступен", "Есть",
-                               format_data(decode_data.get_notBefore()),
-                               format_data(decode_data.get_notAfter()),
-                               days_left(decode_data.get_notAfter()),
-                               signed,
-                               str(decode_data.get_issuer())])
-            except Exception as Ex:
-                print(Ex)
-                result.append([addr, "Доступен", "Нет", "", "", "", "", str(Ex)])
-        except Exception as Ex:
-            print(Ex)
-            result.append([addr, "Не доступен", "", "", "", "", "", str(Ex)])
+        result.append(get_data(addr))
 
     results.append([file, result])
 
